740e00c9d4e828ab364de74c639c485c5513d2d1
Lamb <lambgao@me.com>
2012-11-13 18:19:40 +0800

    配置文件

---
+++ src/test/java/org/mynah/mesh/model/TmItem.java
@@ -0,0 +1,215 @@
+package org.mynah.mesh.model;
+
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.sql.Date;
+import java.sql.Clob;
+import java.sql.Blob;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.Currency;
+
+public class TmItem {
+
+    private Long id;
+    private Short itemId;
+    private Integer amount;
+    private Integer type;
+    private Float score;
+    private Double labourHour;
+    private Character character;
+    private Boolean status;
+    private Byte bytes;
+    private String content;
+    private Date createDate;
+    private BigDecimal bigDecimal;
+    private byte[] byteArray;
+    private Object object;
+    private Time time;
+    private Timestamp timestamp;
+    private Calendar calendar;
+    private Clob Clob;
+    private Blob Blob;
+    private Locale locale;
+    private TimeZone timeZone;
+    private Currency Currency;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Short getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Short itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+
+    public Double getLabourHour() {
+        return labourHour;
+    }
+
+    public void setLabourHour(Double labourHour) {
+        this.labourHour = labourHour;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Byte getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(Byte bytes) {
+        this.bytes = bytes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public BigDecimal getBigDecimal() {
+        return bigDecimal;
+    }
+
+    public void setBigDecimal(BigDecimal bigDecimal) {
+        this.bigDecimal = bigDecimal;
+    }
+
+    public byte[] getByteArray() {
+        return byteArray;
+    }
+
+    public void setByteArray(byte[] byteArray) {
+        this.byteArray = byteArray;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public Clob getClob() {
+        return Clob;
+    }
+
+    public void setClob(Clob clob) {
+        Clob = clob;
+    }
+
+    public Blob getBlob() {
+        return Blob;
+    }
+
+    public void setBlob(Blob blob) {
+        Blob = blob;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public TimeZone getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public Currency getCurrency() {
+        return Currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        Currency = currency;
+    }
+
+}
