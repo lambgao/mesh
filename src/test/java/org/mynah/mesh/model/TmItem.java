0334771631d307a7057f60ebd5fc8c94d7271594
Lamb <lambgao@me.com>
2012-11-14 18:11:01 +0800

    Excel注解定义

--- src/test/java/org/mynah/mesh/model/TmItem.java
+++ src/test/java/org/mynah/mesh/model/TmItem.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mynah.mesh.model;
 
 import java.math.BigDecimal;
@@ -11,8 +26,10 @@ import java.util.Locale;
 import java.util.TimeZone;
 import java.util.Currency;
 
-public class TmItem {
+import org.mynah.mesh.annotation.Excel;
 
+public class TmItem {
+    @Excel(column = 0, title = "编号")
     private Long id;
     private Short itemId;
     private Integer amount;
